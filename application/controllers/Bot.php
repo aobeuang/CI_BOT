f493f93a020025ecd31a4ea37b70a60b51daefac
aobeuang <aob.attaporn@gmail.com>
2019-05-24 05:27:34 +0700

    Update Bot.php

--- application/controllers/Bot.php
+++ application/controllers/Bot.php
@@ -23,7 +23,7 @@ public function __construct()
 	    $arrayHeader = array();
 	    $arrayHeader[] = "Content-Type: application/json";
 	    $arrayHeader[] = "Authorization: Bearer {".LINE_ACCESS_TOKEN."}";
-	    if(strpos($message, 'เทส') !== false){
+	    if(strpos($message, '...') !== false){
 	    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 	    $arrayPostData['messages'][0]['type'] = "text";
 	    $arrayPostData['messages'][0]['text'] = "555";
@@ -36,7 +36,7 @@ public function __construct()
           #ตัวอย่าง Message Type "Location"
     else if(strpos($message, 'เทส') !== false){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]= $this->Line->gProfile($arrayJson['events'][0]['source']['userId']);
+        $arrayPostData['messages'][0] = $this->Line->gProfile($arrayJson['events'][0]['source']['userId']);
 
     }
         
